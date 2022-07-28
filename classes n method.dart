c7ec74ca96ef9a6ea5f1c285017b00b3163e0d9c
Gift Masha <gftman24@gmail.com>
2022-07-28 16:13:21 +0300

    added constructor

--- classes n method.dart
+++ classes n method.dart
@@ -1,19 +1,22 @@
-//creating a class
-//all class names must start with capital letters
-class Person {
-  late String firstname;
-
-  //created a method called displayname
-  displayname() {
-    print(firstname);
-  }
-}
-
 void main() {
   //creating and instance of a class
   //creating a variable to declare an instance of a class
 
-  var person = new Person();
-  person.firstname = 'gift';
+  var person = new Person('gift');
+
   person.displayname();
+}
+
+//creating a class
+//all class names must start with capital letters
+class Person {
+  late String fname;
+  //created a method called displayname
+  displayname() {
+    print(fname);
+  }
+
+  //creating a constructor called person
+  //every time we create an input in the class Person..the input is tagged as firstname.
+  Person(this.fname);
 }
